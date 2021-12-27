f5855ee7bb11d3979727c204be9b804e76ba319e
AhmadS7 <myvirtualrabbit15@gmail.com>
2021-12-27 21:59:00 +0600

    id added

--- houses.js
+++ houses.js
@@ -2,15 +2,17 @@
 
 export default [
   {
+    id: 1,
     picture: '/img/1.jpg',
     type: 'Entire house',
     town: 'New York',
     title: 'Beautiful flat in New York!'
   },
   {
+    id: 2,
     picture: '/img/2.jpg',
     type: 'Entire house',
     town: 'Amsterdam',
     title: 'A flat in Amsterdam with a great view'
   }
-];
+]
