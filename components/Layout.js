006bd36279b505322a12cd440672fe0897cbfe73
AhmadS7 <myvirtualrabbit15@gmail.com>
2021-12-31 18:44:30 +0600

    added

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,28 @@
-export default function Layout(props) {
-  return (
-    <div>
-      <main>{props.content}</main>
+import houses from '../houses.js';
+import House from '../components/House';
+import Layout from '../components/Layout';
+
+const content = (
+  <div>
+    <h2>Places to stay</h2>
+
+    <div className="houses">
+      {houses.map((house, index) => {
+        return <House key={index} {...house} />;
+      })}
     </div>
-  );
+
+    <style jsx>{`
+      .houses {
+        display: grid;
+        grid-template-columns: 49% 49%;
+        grid-template-rows: 300px 300px;
+        grid-gap: 2%;
+      }
+    `}</style>
+  </div>
+);
+
+export default function Home() {
+  return <Layout content={content} />;
 }
