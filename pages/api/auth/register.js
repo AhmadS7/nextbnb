45260dc07c727322e480e3933e59ae526d3d6656
AhmadS7 <myvirtualrabbit15@gmail.com>
2022-02-05 06:13:26 +0600

    added bcrypt

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,9 +1,19 @@
+import { User } from '../../../model.js';
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req, res) => {
+export default async (req, res) => {
   if (req.method !== 'POST') {
     res.status(405).end(); //Method Not Allowed
     return;
   }
-  console.log(req.body);
-  res.end();
+
+  const { email, password, passwordconfirmation } = req.body;
+  if (password !== passwordconfirmation) {
+    res.end(
+      JSON.stringify({ status: 'error', message: 'password do not match' })
+    );
+    return;
+  }
+
+  const user = await User.create({ email, password });
+  res.end(JSON.stringify({ status: 'success', message: 'User added' }));
 };
